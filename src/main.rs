a49b7c139632254708073bf72922b3b4dc97b409
Yufeng Gosling <yufeng_gosling@outlook.com>
2025-03-01 11:09:40 +0800

    get_file function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
+#[warn(dead_code)]
+
+use std::fs;
+use std::io;
+
+// 获取文件
+fn get_file(dir: &str) -> io::Result<Vec<String>> {
+    let mut file_list: Vec<String> = Vec::new();
     
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if let Some(ext) = path.extension() {
+            if ext.to_string_lossy() == "py" {
+                file_list.push(path.to_string_lossy().into_owned());
+            }
+        }
+    }
+    Ok(file_list)
 }
+
+fn main() {}
